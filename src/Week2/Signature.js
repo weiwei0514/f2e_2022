6e7f82eeb81adcf9e5947fb0a5c0f7e9cfa5beac
weiwei0514 <iverson0956@gmail.com>
2022-10-27 18:03:52 +0800

    [Modify] change pen color

--- src/Week2/Signature.js
+++ src/Week2/Signature.js
@@ -8,7 +8,8 @@ const Signature = () => {
   const [color, setColor] = useState("#000")
   const wrapperRef = useRef(null)
   const canvasRef = useRef(null)
-  const ctxRef = useRef(null)
+  const [canvas, setCanvas] = useState(null)
+  const [ctx, setCtx] = useState(null)
   const windowWidth = useWindowDimensions().width
   // 開始繪圖時，將狀態開啟
   const startPosition = (e) => {
@@ -19,8 +20,8 @@ const Signature = () => {
   // 結束繪圖時，將狀態關閉，並產生新路徑
   const finishedPosition = useCallback(() => {
     setIsPainting(false)
-    ctxRef.current.beginPath()
-  }, [])
+    ctx.beginPath()
+  }, [ctx])
 
   // 取得滑鼠 / 手指在畫布上的位置
   const getPaintPosition = useCallback((e) => {
@@ -48,43 +49,48 @@ const Signature = () => {
       const paintPosition = getPaintPosition(e)
 
       // 移動滑鼠位置並產生圖案
-      ctxRef.current.lineTo(paintPosition.x, paintPosition.y)
-      ctxRef.current.stroke()
+      ctx.lineTo(paintPosition.x, paintPosition.y)
+      ctx.stroke()
     },
     [getPaintPosition, isPainting]
   )
   // 重新設定畫布
   const resetCanvas = () => {
-    ctxRef.current.clearRect(
-      0,
-      0,
-      canvasRef.current.width,
-      canvasRef.current.height
-    )
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
   }
 
   // 產生簽名圖檔
   const saveImage = () => {
     // 圖片儲存的類型選擇 png ，並將值放入 img 的 src
-    const newImg = canvasRef.current.toDataURL("image/png")
+    const newImg = canvas.toDataURL("image/png")
     setSignatureImg(newImg)
     localStorage.setItem("img", newImg)
   }
+  // 畫布初始化
+  useEffect(() => {
+    const c = canvasRef.current
+    setCanvas(c)
+    if (c) setCtx(c.getContext("2d"))
+  }, [canvasRef])
 
   // 畫布設定
   useEffect(() => {
-    const canvas = canvasRef.current
-    canvas.width = wrapperRef.current.clientWidth
-    canvas.height = 300
-    canvas.style.width = `${wrapperRef.current.clientWidth}px`
-    canvas.style.height = "300px"
+    if (canvas && ctx) {
+      canvas.width = wrapperRef.current.clientWidth
+      canvas.height = 300
+      canvas.style.width = `${wrapperRef.current.clientWidth}px`
+      canvas.style.height = "300px"
 
-    const ctx = canvas.getContext("2d")
-    ctx.lineWidth = 2
-    ctx.lineCap = "round"
-    ctx.strokeStyle = color
-    ctxRef.current = ctx
-  }, [windowWidth,color])
+      ctx.lineWidth = 2
+      ctx.lineCap = "round"
+    }
+  }, [windowWidth, canvas, ctx])
+
+  useEffect(() => {
+    if (ctx) {
+      ctx.strokeStyle = color
+    }
+  }, [color, ctx])
 
   useEffect(() => {
     const canvas = canvasRef.current
@@ -116,9 +122,21 @@ const Signature = () => {
     <SignatureWrapper ref={wrapperRef}>
       <canvas ref={canvasRef} />
       <div className="color-group">
-        <div className="circle" style={{backgroundColor:"#000"}} onClick={(e)=>setColor(e.target.style.backgroundColor)}/>
-        <div className="circle" style={{backgroundColor:"#1b7ced"}} onClick={(e)=>setColor(e.target.style.backgroundColor)}/>
-        <div className="circle" style={{backgroundColor:"#ff0000"}} onClick={(e)=>setColor(e.target.style.backgroundColor)}/>
+        <div
+          className="circle"
+          style={{ backgroundColor: "#000" }}
+          onClick={(e) => setColor(e.target.style.backgroundColor)}
+        />
+        <div
+          className="circle"
+          style={{ backgroundColor: "#1b7ced" }}
+          onClick={(e) => setColor(e.target.style.backgroundColor)}
+        />
+        <div
+          className="circle"
+          style={{ backgroundColor: "#ff0000" }}
+          onClick={(e) => setColor(e.target.style.backgroundColor)}
+        />
       </div>
       <div className="btn-group">
         <button className="clear" onClick={resetCanvas}>
@@ -139,27 +157,27 @@ const SignatureWrapper = styled.div`
   }
   .btn-group {
   }
-  .color-group{
-    padding:10px;
-    display:flex;
-    .circle{
-      width:30px;
-      height:30px;
-      border-radius:50%;
-      margin-right:10px;
-      cursor:pointer;
-      &:hover{
-        opacity:.7;
+  .color-group {
+    padding: 10px;
+    display: flex;
+    .circle {
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      margin-right: 10px;
+      cursor: pointer;
+      &:hover {
+        opacity: 0.7;
       }
     }
-    .black{
-      background-color:#000;
+    .black {
+      background-color: #000;
     }
-    .blue{
-      background-color:#1b7ced;
+    .blue {
+      background-color: #1b7ced;
     }
-    .red{
-      background-color:#ff0000;
+    .red {
+      background-color: #ff0000;
     }
   }
 `
