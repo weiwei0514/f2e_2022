6b4ec6e587eabd9e24c169e7355b893b2b732331
Eddy_Chou <x34521@gmail.com>
2022-11-14 22:30:19 +0800

    download feature

--- src/Week2/Pdf.js
+++ src/Week2/Pdf.js
@@ -1,63 +1,147 @@
-import React, { useState, useRef } from "react"
-import { pdfjs } from "react-pdf"
-import styled from "styled-components"
+import React, { useState, useRef, useEffect, useCallback } from 'react'
+import { pdfjs } from 'react-pdf'
+import styled from 'styled-components'
+import { fabric } from 'fabric'
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
 
-const Pdf = () => {
-  const [message, setMessage] = useState("")
+const Pdf = (props) => {
+  const { signatureImg } = props
+
+  const [message, setMessage] = useState('')
   const canvasRef = useRef(null)
-  const UploadPdfHandler = (e) => {
-    // 透過 input 所選取的檔案
+  const exportRef = useRef(null)
+
+  const [exportCanvas, setExportCanvas] = useState(null)
+
+  const handlePdf = (e) => {
     const file = e.target.files[0]
-    if (file.type === "application/pdf") {
+
+    if (file.type === 'application/pdf') {
       // 判斷檔案大小
       if (file.size > 10000000) {
-        setMessage("檔案超過10Mb")
+        setMessage('檔案超過10Mb')
+
         return
       }
-      // 產生fileReader物件
       const fileReader = new FileReader()
-      // 將資料做處理
-      fileReader.readAsArrayBuffer(file)
 
-      fileReader.addEventListener("load", () => {
-        // 獲取readAsArrayBuffer產生的結果，並用來渲染PDF
-        const pdfData = new Uint8Array(fileReader.result)
+      fileReader.onload = function () {
+        const pdfData = new Uint8Array(this.result)
         const loadingTask = pdfjs.getDocument({ data: pdfData })
+
         loadingTask.promise.then(
           (pdf) => {
             const pageNumber = 1
+
             pdf.getPage(pageNumber).then((page) => {
-              const scale = 1.5
-              const viewport = page.getViewport({ scale: scale })
-              const ctx = canvasRef.current.getContext('2d')
+              const scale = 1
+              const viewport = page.getViewport({ scale })
+
               canvasRef.current.height = viewport.height
               canvasRef.current.width = viewport.width
+
               const renderContext = {
-                canvasContext: ctx,
-                viewport: viewport,
+                viewport,
+                canvasContext: canvasRef.current.getContext('2d'),
               }
-              page.render(renderContext)
+
+              const renderTask = page.render(renderContext)
+
+              renderTask.promise.then(() => {
+                console.log('Page rendered')
+              })
             })
           },
-          (reason) => {
-            // PDF loading error
-            console.error(reason)
-          }
+          (reason) => console.error(reason)
         )
-      })
+      }
+
+      fileReader.readAsArrayBuffer(file)
     } else {
-      setMessage("請上傳pdf檔案")
+      setMessage('請上傳pdf檔案')
     }
   }
 
+  const handleExport = () => {
+    const bgImg = canvasRef.current.toDataURL()
+
+    fabric.Image.fromURL(bgImg, (img) => {
+      exportCanvas.setBackgroundImage(bgImg).renderAll()
+      exportCanvas.setHeight(img.height)
+      exportCanvas.setWidth(img.width)
+      scaleAndPositionImage(img)
+    })
+
+    fabric.Image.fromURL(signatureImg, (img) => {
+      img.scaleToWidth(100)
+      img.scaleToHeight(100)
+      exportCanvas.add(img).renderAll()
+    })
+  }
+
+  /** 縮放 */
+  const scaleAndPositionImage = (bgImage) => {
+    const canvasWidth = canvasRef.current.width
+    const canvasHeight = canvasRef.current.height
+
+    exportCanvas.setWidth(canvasWidth)
+    exportCanvas.setHeight(canvasHeight)
+
+    const canvasAspect = canvasWidth / canvasHeight
+    const imgAspect = bgImage.width / bgImage.height
+    let left, top, scaleFactor
+
+    if (canvasAspect >= imgAspect) {
+      scaleFactor = canvasWidth / bgImage.width
+      left = 0
+      top = -(bgImage.height * scaleFactor - canvasHeight) / 2
+    } else {
+      scaleFactor = canvasHeight / bgImage.height
+      top = 0
+      left = -(bgImage.width * scaleFactor - canvasWidth) / 2
+    }
+
+    exportCanvas.setBackgroundImage(
+      bgImage,
+      exportCanvas.renderAll.bind(exportCanvas),
+      {
+        top: top,
+        left: left,
+        originX: 'left',
+        originY: 'top',
+        scaleX: scaleFactor,
+        scaleY: scaleFactor,
+      }
+    )
+  }
+
+  /** 下載 */
+  const download = () => {
+    const dataURL = exportCanvas.toDataURL({ format: 'png' })
+    const link = document.createElement('a')
+    link.download = 'my-image.png'
+    link.href = dataURL
+    link.target = '_blank'
+    document.body.appendChild(link)
+    link.click()
+    link.parentNode.removeChild(link)
+  }
+
+  useEffect(() => {
+    const c = new fabric.Canvas(exportRef.current)
+
+    setExportCanvas(c)
+  }, [exportRef])
 
   return (
     <PdfWrapper>
-      <input accept=".pdf" type="file" onChange={UploadPdfHandler} />
+      <input accept=".pdf" type="file" onChange={handlePdf} />
       <p>{message}</p>
-      <canvas ref={canvasRef} />
+      <button onClick={handleExport}>export</button>
+      <button onClick={download}>download</button>
+      <canvas style={{ display: 'none' }} ref={canvasRef} />
+      <canvas ref={exportRef} />
     </PdfWrapper>
   )
 }
--- src/Week2/index.js
+++ src/Week2/index.js
@@ -1,18 +1,20 @@
-import React from "react"
-import styled from "styled-components"
+import React, { useState } from 'react'
+import styled from 'styled-components'
 // components
-import Signature from "./Signature"
-import Pdf from "./Pdf"
+import Signature from './Signature'
+import Pdf from './Pdf'
 const Week2 = () => {
+  const [signatureImg, setSignatureImg] = useState(null)
 
   return (
     <Week2Wrapper>
-      <Signature/>
-      <Pdf/>
+      <Signature
+        signatureImg={signatureImg}
+        setSignatureImg={setSignatureImg}
+      />
+      <Pdf signatureImg={signatureImg} />
     </Week2Wrapper>
   )
 }
-const Week2Wrapper = styled.div`
-
-`
+const Week2Wrapper = styled.div``
 export default Week2
